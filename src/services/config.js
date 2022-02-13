c51991fd73d7a5c80bf0d0ba29c25575a863ee53
Andrew <andrewmoplas@gmail.com>
2022-02-13 17:10:44 +0800

    set api url

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,8 @@
-const apiUrl = 'http://127.0.0.1:8000/v1/';
+// TODO: Temporarily set API Url
+// const apiUrl = 'http://127.0.0.1:8000/v1/';
 
 const config = {
-  API_URL: apiUrl,
+  API_URL: `http://${window.location.hostname}:8000/v1/`,
 };
 
 export default config;
